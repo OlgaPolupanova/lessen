3794cc422002142fc230932aca0d483382eb6f52
n.polupanov <polupanov_90@mail.ru>
2022-10-13 18:03:11 +0300

    home work 20

---
+++ lesson_20/script.js
@@ -0,0 +1,21 @@
+
+// безконечный цикл for
+for ( ; true; ) {
+  console.log('Hello world')
+}
+// цикл for который никогда не выполнится
+for ( ; false; ) {
+  console.log('Hello world')
+}
+
+// цикл for
+for (var x = 0; x < 10 ; x++) {
+  console.log('Hello world')
+}
+
+// проход по массиву
+const colors = ['red', 'green', 'blue'];
+for (var y = 0; y < colors.length; y++) {
+  console.log(colors[y]);
+}
+
